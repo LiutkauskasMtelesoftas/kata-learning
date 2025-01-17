bfec609a158f377dd361c86a0727bcea8c6726d7
Mindaugas Liutkauskas <mindaugas.liutkauskas@telesotas.com>
2025-01-17 09:32:38 +0200

    chore: add unit tests for concert quality

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,11 +5,29 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_quality_should_be_less_than_50(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual(50, items[0].quality)
+        
+    def test_quality_to_concert_should_increase_by_2_when_sell_in_is_10_or_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(12, items[0].quality)
+    
+    def test_quality_to_concert_should_increase_by_3_when_sell_in_is_5_or_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(13, items[0].quality)
+    
+    def test_quality_to_concert_should_be_0_when_sell_in_is_0(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
 
         
 if __name__ == '__main__':
