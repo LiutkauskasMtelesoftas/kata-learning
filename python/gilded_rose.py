0b0ca1270b52fd6750e782c13e2567ddd0528ee5
Mindaugas Liutkauskas <mindaugas.liutkauskas@telesotas.com>
2025-02-14 10:34:19 +0200

    chore: try using strategy design pattern

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,38 +7,9 @@ class GildedRose(object):
 
     def update_quality(self) -> None:
         
-        brie = "brie"
-        concert_substrings = ["concert", "backstage", "pass"]
-        sulfuras = "sulfuras"
-        quality_items = [brie, sulfuras] + concert_substrings
-        
         for item in self.items:
-            lower_name = item.name.lower()
-            if lower_name.__contains__(sulfuras):
-                continue
-            if not any(special in lower_name for special in quality_items):
-                self.decrease_quality(item)
-            else:
-                self.increase_quality(item,concert_substrings)
-            if item.sell_in <= 0:
-                if not any(special in lower_name for special in quality_items):  
-                    self.decrease_quality(item)
-                elif any(concert in lower_name for concert in concert_substrings):
-                    item.quality = 0
-                item.sell_in -= 1
-
-    def decrease_quality(self, item):
-        conjured = "conjured".lower()
-        item.quality -= 1 if item.quality > 0 else 0
-        item.quality -= 1 if conjured in item.name.lower() and item.quality > 0 else 0
-        item.sell_in -= 1 if item.sell_in > 0 else 0
-
-    def increase_quality(self, item, concert_substrings):
-        item.quality +=1 if item.quality < 50 else 0
-        if any(concert in item.name.lower() for concert in concert_substrings):
-            item.quality += 1 if item.sell_in < 11 and item.quality < 50 else 0
-            item.quality += 1 if item.sell_in < 6 and item.quality < 50 else 0
-        item.sell_in -= 1 if item.sell_in >= 0 else 0
+            strategy = StrategyContext.get_strategy(item)
+            strategy.update(item)
 
 class Item:
     def __init__(self, name, sell_in, quality) -> None:
@@ -48,3 +19,49 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+
+class UpdateStrategyInterface:
+    def update(self, item):
+        pass
+
+class NormalItemUpdateStrategy(UpdateStrategyInterface):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality -= 1 if item.quality > 0 else 0
+        item.quality -= 1 if item.sell_in < 0 and item.quality > 0 else 0
+
+class AgedBrieUpdateStrategy(UpdateStrategyInterface):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality += 1 if item.quality < 50 else 0
+
+class SulfurasUpdateStrategy(UpdateStrategyInterface):
+    def update(self, item):
+        pass
+    
+class BackstagePassUpdateStrategy(UpdateStrategyInterface):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality += 1 if item.quality < 50 else 0
+        item.quality += 1 if item.sell_in < 11 and item.quality < 50 else 0
+        item.quality += 1 if item.sell_in < 6 and item.quality < 50 else 0
+        item.quality = 0 if item.sell_in < 0 else item.quality
+
+class ConjuredUpdateStrategy(UpdateStrategyInterface):
+    def update(self, item):
+        item.sell_in -= 1
+        item.quality -= 2 if item.quality > 0 else 0
+        item.quality -= 2 if item.sell_in < 0 and item.quality > 0 else 0
+        
+class StrategyContext:
+    def get_strategy(item):
+        if "aged brie" in item.name.lower():
+            return AgedBrieUpdateStrategy()
+        elif "sulfuras" in item.name.lower():
+            return SulfurasUpdateStrategy()
+        elif "backstage pass" in item.name.lower():
+            return BackstagePassUpdateStrategy()
+        elif "conjured" in item.name.lower():
+            return ConjuredUpdateStrategy()
+        else:
+            return NormalItemUpdateStrategy()
