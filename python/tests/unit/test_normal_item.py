36de8b8c342aa59bf8a4ed68c3995966fe3c67d4
Mindaugas Liutkauskas <114151476+LiutkauskasMtelesoftas@users.noreply.github.com>
2025-02-28 11:57:34 +0200

    Update python/tests/unit/test_normal_item.py

--- python/tests/unit/test_normal_item.py
+++ python/tests/unit/test_normal_item.py
@@ -7,7 +7,6 @@ class NormalItemUpdateStrategyTest(unittest.TestCase):
             (Item("product", 5, 10), [4, 9]),
             (Item("product", 0, 10), [-1, 8]),
             (Item("product", 5, 1), [4, 0]),
-            (Item("product", 0, 10), [-1, 8]),
             (Item("product", 5, 0), [4, 0])
         ]
         for item, expected in test_cases:
